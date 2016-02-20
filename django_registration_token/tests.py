17e29eed681b546c7d65d461d1045c6377b0993f
Santiago Vargas de Kruijf <santiago.vdk@gmail.com>
2016-02-20 00:39:26 -0600

    Added basic testing

--- django_registration_token/tests.py
+++ django_registration_token/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django_registration_token.models import InvitationCode
+
+class InvitationCodeTestCase(TestCase):
+    def test_token_generation(self):
+        i = 5
+        while i > 0:
+            try:
+                token = InvitationCode()
+                token.save()
+                print(token.code)
+                print('\x1b[0m')
+                i -= 1
+            except:
+                self.assertEqual(i,0)
+        self.assertEqual(i,0)
